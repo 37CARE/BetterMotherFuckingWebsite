48ea4d62181e7f68d509f5981868981a9bc4bef1
M'Hamed larbi <mhamedlarbi@MacBook-Pro-de-MHamed.local>
2019-03-28 00:57:42 +0100

    test

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.querySelector('html').addEventListener('click', function(click) {
+    alert( "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+});
+if ( confirm( "Hey le site xxxvidsxxx est trop bien. Viens dessus stp please" ) ) {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" ;
+    // Code à éxécuter si le l'utilisateur clique sur "OK"
+} else {
+    // Code à éxécuter si l'utilisateur clique sur "Annuler" 
+}
